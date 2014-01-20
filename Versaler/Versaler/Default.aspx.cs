45054169bd2067ec16836c5643b8eb8b8d9c88b0
em222iv <em222iv@student.lnu.se>
2014-01-20 18:32:30 +0100

    versaler = done

---
+++ Versaler/Versaler/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Versaler
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Button1.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Result.Text = "Antalet versaler är: " + Model.TextAnalyzer.GetNumberofCapitals(Text.Text).ToString();
+            Text.Enabled = false;
+            Button.Visible = false;
+            Button1.Visible = true;
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Result.Text = "";
+            Text.Enabled = true;
+            Button.Visible = true;
+        }
+    }
+}
---
+++ Versaler/Versaler/Model/TextAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+using System.Web;
+
+namespace Versaler.Model
+{
+    static class TextAnalyzer
+    {
+        public static int GetNumberofCapitals(string Text) {
+
+            int numberOfChars;
+            Regex Reg = new Regex("[A-ZÅÄÖ]");
+            numberOfChars = Reg.Matches(Text).Count;
+            
+            return numberOfChars;
+
+           /* int numberOfChars = 0;
+        
+            char[] array = Text.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {  
+                if (array[i] == Char.ToUpper(array[i]))
+                {
+                    numberOfChars++;
+                }
+            }
+            return numberOfChars;*/
+        }
+    }
+}
